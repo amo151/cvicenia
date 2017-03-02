6f480cd06be6e3761fc6761acf374621f9021df3
amo151 <jano.balga@gmail.com>
2017-03-02 10:48:47 +0100

    posledna komita z hodiny

---
+++ uloha01/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+class Stack {
+private:
+	int ukazatel;//ukazuje kde v poli je ulozena posledna hodnota
+	int *pole;
+public:
+	Stack(int pocet);
+	~Stack();
+	int pop();
+	void push(int hodnota);
+
+};
+
+int main()
+{
+	Stack zasobnik(20);
+
+	zasobnik.push(12);
+	zasobnik.push(134);
+	zasobnik.push(98);
+
+	cout << zasobnik.pop() << endl;
+	cout << zasobnik.pop() << endl;
+	cout << zasobnik.pop() << endl;
+
+
+	return 0;
+}
+
+Stack::Stack(int pocet)
+{
+	pole = new int[pocet];
+	ukazatel = -1;
+}
+
+int Stack::pop()
+{
+	int prvok = pole[ukazatel];
+	pole[ukazatel] = 0;
+	--ukazatel;
+	return prvok;
+}
+
+void Stack::push(int hodnota)
+{
+	pole[++ukazatel] = hodnota;
+}
+Stack::~Stack()
+{
+	delete pole;
+}
