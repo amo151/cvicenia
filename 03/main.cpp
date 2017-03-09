9dde68e4640ea0964695a6c4064afba1d32a4366
amo151 <jano.balga@gmail.com>
2017-03-09 09:47:16 +0100

    skuska

--- 03/main.cpp
+++ 03/main.cpp
@@ -1,17 +1,39 @@
 #include <iostream>
+
 using namespace std;
-class A
-{
-	int a,b;
+
+class A {
+	int a, b;
 public:
-	A(int h):a(h),b(h){}
-	int vratA()const {return a;}
-	int vratB()const {return b;}
+	A(int h) : a(h), b(h){}
+
+	virtual int vratA() const{return a;}
+	int vratB() const{return b;}
+
+
+};
+
+class B:public A {
+	mutable int b;//ak v const funkcii chcem menit tak musi byt mutable
+
+public:
+	B(int h) : A(h), b(h){}
+	int vratA()const {return b++;}
+
+	static double pi() {return 3.14;}
+
+
 };
 
 int main()
 {
 	A a(10);
-	cout << "a = " << a.vratA() <<endl<< "b = " << a.vratB()<<endl;
+	cout << "a = " << a.vratA() << endl << "b = " << a.vratB() << endl;
+
+	B b(33);
+	cout << "b = " << b.vratA() << endl;
+
+	cout << "pi = " << B::pi() << endl;
+
 	return 0;
 }
